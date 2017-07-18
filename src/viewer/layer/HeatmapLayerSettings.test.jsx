d400526b3ddfee69c75f5df9d1e76fc6fe586a5c
Bjørn Reppen <bjreppen@gmail.com>
2017-07-18 12:08:42 +0200

    Layer settings tweaks

--- src/viewer/layer/HeatmapLayerSettings.test.jsx
+++ src/viewer/layer/HeatmapLayerSettings.test.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
-import HeatMapLayerSettings from './HeatmapLayerSettings'
+import Bars3DLayerSettings from './Bars3DLayerSettings'
 
 it('loading standard tile', () => {
   const tree = renderer.create(
     <MuiThemeProvider>
-      <HeatMapLayerSettings fillOpacity={0.1} radius={0.2} coverage={0.3} />
+      <Bars3DLayerSettings fillOpacity={0.1} radius={0.2} coverage={0.3} />
     </MuiThemeProvider>
       )
   expect(tree.toJSON()).toMatchSnapshot()
