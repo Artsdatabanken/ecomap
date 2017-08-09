6c90d40749b13e5691107c8ead0965c0db73d9fa
Bjørn Reppen <bjreppen@gmail.com>
2017-08-09 23:51:08 +0200

    Make map of intensity

--- src/map/layer/webgl/heatmapFromPoints-layer/heatmapFromPoints-layer-fragment.glsl.js
+++ src/map/layer/webgl/heatmapFromPoints-layer/heatmapFromPoints-layer-fragment.glsl.js
@@ -11,12 +11,13 @@ uniform sampler2D colorRamp;
 
 void main(void) {
   float distToCenter = length(unitPosition);
-
+  float intensity = exp(-pow(distToCenter, 2.0) * 20.0)*0.08;
   if (distToCenter <= 1.0) {
-//    gl_FragColor = vColor;
-    gl_FragColor = texture2D(colorRamp, vec2(distToCenter,255.0*distToCenter));
+    gl_FragColor = vec4(intensity, intensity, intensity, intensity*5.0);
+//    gl_FragColor += vColor;
+    //    gl_FragColor = texture2D(colorRamp, vec2(distToCenter, 1.0));
 
-//    gl_FragColor.a *= exp(-pow(distToCenter, 2.0)/0.3);
+//    gl_FragColor.a = exp(-pow(distToCenter, 2.0) / 0.3);
   } else {
     discard;
   }
