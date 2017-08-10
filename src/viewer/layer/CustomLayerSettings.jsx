f5d6b0879f5bb2706dfb326e45809a7e1ec50b7c
Bjørn Reppen <bjreppen@gmail.com>
2017-08-10 02:41:08 +0200

    Heatmap on map

--- src/viewer/layer/CustomLayerSettings.jsx
+++ src/viewer/layer/CustomLayerSettings.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import HeatmapLayerSettings from './HeatmapLayerSettings'
 import HexagonLayerSettings from './HexagonLayerSettings'
 import ScatterplotLayerSettings from './ScatterplotLayerSettings'
 
 const CustomLayerSettings = (props) => {
   switch (props.renderMethod) {
+    case 'heatmap' : return <HeatmapLayerSettings {...props} />
     case 'scatterplot' : return <ScatterplotLayerSettings {...props} />
-    case 'hexagon' : return <HexagonLayerSettings {...props} />
+case 'hexagon' : return <HexagonLayerSettings {...props} />
     default: return <div>{this.props.renderMethod}</div>
   }
 }
