d4f131645ab94414b4935fb8820ba356b9a1a9ae
Bjørn Reppen <bjreppen@gmail.com>
2017-08-13 11:33:31 +0200

    Add tests for heatmap settings

---
+++ src/viewer/layer/HexagonLayerSettings.test.1.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import muiTheme from '../../theme_jest'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+import HexagonLayerSettings from './HexagonLayerSettings'
+
+it('renders ok', () => {
+  const tree = renderer.create(
+    <MuiThemeProvider muiTheme={muiTheme}>
+      <HexagonLayerSettings fillOpacity={0.1} radius={0.2} coverage={0.3} />
+    </MuiThemeProvider>
+      )
+  expect(tree.toJSON()).toMatchSnapshot()
+})
