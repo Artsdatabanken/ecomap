c38c4900333d0fe6f1fccd27e2747b6ea76c7294
Bjørn Reppen <bjreppen@gmail.com>
2017-08-16 13:08:14 +0200

    Add stuff cards

--- src/viewer/layer/add/AddLayer.jsx
+++ src/viewer/layer/add/AddLayer.jsx
@@ -1,57 +1,33 @@
 import React from 'react'
 import { Paper } from 'material-ui'
-import { Tabs, Tab } from 'material-ui/Tabs'
-import FontIcon from 'material-ui/FontIcon'
+import { Card, CardMedia, CardTitle, CardText } from 'material-ui/Card'
 import Liste from './Liste'
 import TextField from 'material-ui/TextField'
-import localDrink from 'material-ui/svg-icons/maps/local-drink'
-import SelectSpecies from './species/SelectSpecies'
 
 class AddLayer extends React.Component {
   render () {
     return (
-      <div style={{}}>
-        <Paper
-          rounded
-          zDepth={4}
-          style={{
-            position: 'absolute',
-            height: 'auto',
-            bottom: 0,
-            top: 0,
-            left: 0,
-            right: 0
-          }}
-        >
-          <Tabs
-            onChange={(a, tab, b) => {
-              console.log(b.props)
-              this.setState({ activeTab: tab })
-            }}
-          >
-            <Tab
-              icon={
-                <FontIcon className='muidocs-icon-action-home'>
-                  Species
-                </FontIcon>
-              }
-            >
-              <SelectSpecies onClick={species => this.props.onClick(species)} />
-            </Tab>
-            <Tab icon={<localDrink>Environment</localDrink>} />
-            <Tab
-              icon={
-                <FontIcon className='muidocs-icon-action-home'>Nature</FontIcon>
-              }
-            >
-              <Nature />
-            </Tab>
-          </Tabs>
-        </Paper>
-      </div>
+      <Paper>
+        <div style={{display: 'flex'}}>
+          <Category title='Species' subtitle='Lorem ipsum dolor sit amet consectetur.' imageUrl='https://farm5.staticflickr.com/4107/4839886016_d11b6d2cdf.jpg' />
+          <Category title='Naturestuffs' subtitle='Lorem ipsum dolor sit amet consectetur.' imageUrl='http://artsdatabanken.no/Media/F1698?mode=480x480' />
+          <Category title='Environmental' subtitle='Lorem ipsum dolor sit amet consectetur.' imageUrl='https://upload.wikimedia.org/wikipedia/commons/6/60/SanBernardinoMountains8000feetsign.JPG' />
+        </div>
+      </Paper>
     )
   }
 }
+
+const Category = ({ title, subtitle, imageUrl }) =>
+  <Card style={{width: '400px', margin: '8px', flexDirection: 'column' }}>
+    <CardMedia
+      overlay={<span >
+        <CardTitle titleColor='#eee' title={title} subtitle={subtitle} subtitleColor='#bbb' />
+      </span>
+      }>
+      <img src={imageUrl} />
+    </CardMedia>
+  </Card>
 
 const Nature = () =>
   <div style={{ padding: 10 }}>
