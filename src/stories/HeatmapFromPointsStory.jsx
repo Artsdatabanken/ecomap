5d1cfebf3171016df319fc2470dc7615ec688243
Bjørn Reppen <bjreppen@gmail.com>
2017-08-16 19:43:21 +0200

    remove dupe unused color ramp files.

--- src/stories/HeatmapFromPointsStory.jsx
+++ src/stories/HeatmapFromPointsStory.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import HeatmapFromPointsLayer from '../map/layer/HeatmapFromPoints'
 import alces from '../../data/sample/artskart_48103.json'
 import readGeoJsonPoints from '../translate/GeoJson.js'
-import viridis from '../graphics/color/ramps/viridis.json'
 
 const viewport = {
   width: 900,
@@ -20,7 +19,7 @@ export default HeatmapFromPointsStory =>
   <div style={{ backgroundColor: '#cdcdcd' }}>
     <HeatmapFromPointsLayer
       data={points}
-      colorRamp={viridis}
+      colorRamp='viridis'
       fillOpacity={0.919}
       radius={0.35}
       width={1.0}
