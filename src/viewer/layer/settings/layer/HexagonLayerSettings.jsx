48fad2a2013ae78b3630d87c4c6ce16ded3c284d
Bjørn Reppen <bjreppen@gmail.com>
2017-08-17 01:10:32 +0200

    Use ramps on hexagon layer

--- src/viewer/layer/settings/layer/HexagonLayerSettings.jsx
+++ src/viewer/layer/settings/layer/HexagonLayerSettings.jsx
@@ -3,16 +3,22 @@ import ActionOpacity from 'material-ui/svg-icons/action/opacity'
 import ActionAspectRatio from 'material-ui/svg-icons/action/aspect-ratio'
 import ActionAllOut from 'material-ui/svg-icons/action/all-out'
 import SliderSetting from '../SliderSetting'
+import ColorRamp from '../ColorRamp'
 
 export default class HexagonLayerSettings extends React.Component {
   render () {
     const {fillOpacity, coverage, radius, elevationMin, elevationMax,
-      lowerPercentile, upperPercentile, onChange} = this.props
+      lowerPercentile, colorRamp, upperPercentile, onChange} = this.props
     return (<div>
+      <ColorRamp
+        value={colorRamp}
+        icon={<ActionOpacity />}
+        onChange={value => onChange('colorRamp', value)}
+        />
+      <SliderSetting title='Opacity' value={fillOpacity} icon={<ActionOpacity />} onChange={value => onChange('fillOpacity', value)} />
       <SliderSetting title='Bin radius' value={radius} icon={<ActionAllOut />} onChange={value => onChange('radius', value)} />
       <SliderSetting title='Lower percentile' value={lowerPercentile} icon={<ActionAllOut />} onChange={value => onChange('lowerPercentile', value)} />
       <SliderSetting title='Upper percentile' value={upperPercentile} icon={<ActionAllOut />} onChange={value => onChange('upperPercentile', value)} />
-      <SliderSetting title='Opacity' value={fillOpacity} icon={<ActionOpacity />} onChange={value => onChange('fillOpacity', value)} />
       <SliderSetting title='Coverage' value={coverage} icon={<ActionAspectRatio />} onChange={value => onChange('coverage', value)} />
       <SliderSetting title='Elevation min' value={elevationMin} icon={<ActionAllOut />} onChange={value => onChange('elevationMin', value)} />
       <SliderSetting title='Elevation max' value={elevationMax} icon={<ActionAllOut />} onChange={value => onChange('elevationMax', value)} />
