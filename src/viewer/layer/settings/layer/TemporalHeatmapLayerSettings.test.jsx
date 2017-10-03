da8fce701f96b1ca1cc0b3ff6c576aee22b26c01
Bjørn Reppen <bjreppen@gmail.com>
2017-10-03 08:32:12 +0200

    Improve test coverage

--- src/viewer/layer/settings/layer/TemporalHeatmapLayerSettings.test.jsx
+++ src/viewer/layer/settings/layer/TemporalHeatmapLayerSettings.test.jsx
@@ -3,12 +3,12 @@ import renderer from 'react-test-renderer'
 import muiTheme from '../../../../theme_jest'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
-import HeatmapLayerSettings from './HeatmapLayerSettings'
+import TemporalHeatmapLayerSettings from './TemporalHeatmapLayerSettings'
 
 it('renders ok', () => {
   const tree = renderer.create(
     <MuiThemeProvider muiTheme={muiTheme}>
-      <HeatmapLayerSettings fillOpacity={0.1} radiusScale={0.2} height={0.3} colorRamp='viridis' />
+      <TemporalHeatmapLayerSettings fillOpacity={0.1} radiusScale={0.2} height={0.3} colorRamp='viridis' />
     </MuiThemeProvider>
       )
   expect(tree.toJSON()).toMatchSnapshot()
